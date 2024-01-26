71079f33e44ee0f8c27d7a3bca80f777ef0a9b83
AnKiTu03 <109533077+AnKiTu03@users.noreply.github.com>
2024-01-26 13:42:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def handle_userinput(user_question):
 
 
 def main():
-    pdf_docs = "career-break (1).pdf"
+    pdf_docs = "Cleaned_Midlife_Career_Moves_QA (1).pdf"
     load_dotenv()
     st.set_page_config(page_title="Farmers Chatbot", page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
